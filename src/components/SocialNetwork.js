5bdfd339dc6276a45ddaf89c639cb514ce11f14f
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-03-26 17:30:05 +0200

    socialnetwork component

---
+++ src/components/SocialNetwork.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from 'react';
+
+const SocialNetwork = () => {
+//   const anim = () => {
+// logique du useEffect mais il faut ajouter anim a chaque a
+//   };
+  useEffect(() => {
+    const icons = document.querySelectorAll('.social-network a');
+    icons.forEach((link) => {
+      link.addEventListener('mouseover', (e) => {
+        link.style.transform = `translate(${e.offsetX - 20}px, ${
+          e.offsetY - 13
+        }px)`;
+      });
+
+      link.addEventListener('mouseleave', () => {
+        link.style.transform = '';
+      });
+    });
+  }, []);
+
+  return (
+    <div className="social-network">
+      <ul className="content">
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+        //   onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-facebook"></i>
+          </li>
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+        //   onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-twitter"></i>
+          </li>
+        </a>
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+        //   onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-instagram"></i>
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialNetwork;
